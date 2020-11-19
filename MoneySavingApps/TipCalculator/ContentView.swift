a8c6cb9ab8d951335705236a5a2843d1943953fc
Dexter <31446927+itsdexter66@users.noreply.github.com>
2020-11-19 13:50:41 +0100

    Added Ios and Macos apps
    Added the project folder for the SavingsApp. this folder contains the proj file for Xcode and the Ios and Macos app folders

---
+++ MoneySavingApps/TipCalculator/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  TipCalculator
+//
+//  Created by Dexter Krijgsman on 12/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var total = ""
+    @State var savingPercent: Double = 15.0
+    
+    var body: some View {
+        
+            VStack {
+                Text("Monthly saving calculator")
+                    .font(.title)
+                    .bold()
+                
+                HStack {
+                    Text("€")
+                    TextField("Monthly income", text: $total)
+                        .border(Color.black, width: 0.5)
+                        .keyboardType(.decimalPad)
+                }
+                .padding()
+                
+                HStack{
+                    Slider(value: $savingPercent, in: 1...100, step: 1.0)
+                    Text("\(Int(savingPercent))%")
+                }
+                .padding()
+                
+                if let totalNum = Double(total){
+                    Text("Amount to set aside this month: €\(totalNum * savingPercent / 100, specifier: "%0.2f")")
+                    Text("YearlySavings: €\(totalNum * savingPercent / 100 * 12, specifier: "%0.2f")")
+                    Text("Ten years of saving: €\(totalNum * savingPercent / 100 * 120, specifier: "%0.2f")")
+                } else{
+                    Text("Please enter a number")
+                    Text("")
+                }
+            }
+        }
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ MoneySavingApps/TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Dexter Krijgsman on 12/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
